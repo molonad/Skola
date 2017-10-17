c100e06800b90f088e791dac6278c87c736c8766
unknown <m.sracek@seznam.cz>
2017-10-17 08:17:06 +0200

    prvniui

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,6 +12,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import logika.IHra;
+import logika.Hra;
+import uiText.TextoveRozhrani;
 
 /**
  *
@@ -22,12 +25,14 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Start adventura");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                IHra hra = new Hra();
+                TextoveRozhrani textoveRozhrani = new TextoveRozhrani(hra);
+                textoveRozhrani.hraj();
             }
         });
         
@@ -36,7 +41,7 @@ public class Main extends Application {
         
         Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Moje adventura");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -45,7 +50,18 @@ public class Main extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        if(args.length == 0){
+            launch(args);
+        }else{
+            if (args[0].equals("-text")) {
+                IHra hra = new Hra();
+                TextoveRozhrani textoveRozhrani = new TextoveRozhrani(hra);
+                textoveRozhrani.hraj();
+            }else{
+                System.out.println("Neplatny parametr");
+                System.exit(1);
+            }
+        }
     }
     
 }
