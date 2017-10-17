e962f8cff9802edb6de9d5c405f96dabb9562d62
unknown <m.sracek@seznam.cz>
2017-10-17 09:01:59 +0200

    running UI

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,9 +8,18 @@ package main;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import logika.IHra;
 import logika.Hra;
@@ -24,22 +33,56 @@ public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Start adventura");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
+ 
                 IHra hra = new Hra();
-                TextoveRozhrani textoveRozhrani = new TextoveRozhrani(hra);
-                textoveRozhrani.hraj();
-            }
-        });
+                BorderPane borderPane = new BorderPane();
+                
+                TextArea centerText = new TextArea();
+                centerText.setText(hra.vratUvitani());
+                borderPane.setCenter(centerText);
+                
+                Label zadejPrikazLabel = new Label("Zadej prikaz");
+                zadejPrikazLabel.setFont(Font.font("Arial", FontWeight.BOLD, 16));
+             
+                
+                
+                TextField zadejPrikazTextField = new TextField("Sem zadej prikaz");
+                
+                zadejPrikazTextField.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                       
+                       String zadanyPrikaz = zadejPrikazTextField.getText();
+                       String odpoved = hra.zpracujPrikaz(zadanyPrikaz);
+                       
+                       centerText.appendText("\n"+ zadanyPrikaz +"\n");
+                        centerText.appendText("\n"+ odpoved +"\n");
+                        
+                        zadejPrikazTextField.setText("");
+                        
+                        if (hra.konecHry()) {
+                            zadejPrikazTextField.setEditable(false);
+                        }
+                        
+                    }
+                });
+                
+                FlowPane dolniPanel = new FlowPane();
+                dolniPanel.setAlignment(Pos.CENTER);
+                dolniPanel.getChildren().addAll(zadejPrikazLabel, zadejPrikazTextField);
+                
+                
+                
+                
+                borderPane.setBottom(dolniPanel);
+                
+                
+       //         TextoveRozhrani textoveRozhrani = new TextoveRozhrani(hra);
+       //         textoveRozhrani.hraj();
+
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
         
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(borderPane, 400, 350);
         
         primaryStage.setTitle("Moje adventura");
         primaryStage.setScene(scene);
